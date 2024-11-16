8f78879eb71f895ffbef3a7ecb2b5e1ecb6a6b5a
Richard <richard.bubish@gmail.com>
2024-11-16 21:31:11 +0200

    Adding language and theme switch

--- ricardsbubisslv/src/App.vue
+++ ricardsbubisslv/src/App.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
-import { watch } from 'vue'
+import { computed, watch } from 'vue'
 import { useRoute } from 'vue-router'
+import { useTheme } from 'vuetify';
+
+import { mdiTranslate, mdiWhiteBalanceSunny, mdiMoonWaningCrescent } from '@mdi/js'
 
 import { i18n } from '@/i18n'
 
+const theme = useTheme()
 const route = useRoute()
+
+function toggleTheme(){
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+
+function changeLanguage(languageCode: any){
+  i18n.global.locale.value = languageCode
+}
+
+const themeButton = computed(() => {
+  return theme.global.current.value.dark ? mdiWhiteBalanceSunny : mdiMoonWaningCrescent
+})
+
+const languages = [
+  { title: 'LV',
+    code: 'lv'
+  },
+  { title: 'EN',
+    code: 'en'
+  }
+]
 
 watch(
   route,
@@ -20,7 +45,30 @@ watch(
     <v-app-bar>
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <v-toolbar-title>ricardsbubiss</v-toolbar-title>
+    <v-btn :icon="themeButton" @click="toggleTheme"/>
+    <v-menu location="bottom">
+      <template v-slot:activator="{ props }">
+        <v-btn
+        dark
+        v-bind="props"
+        :icon="mdiTranslate"
+        >
+    </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+        v-for="(item,index) in languages"
+        :key="index"
+        @click="changeLanguage(item.code)"
+        >
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
+  <v-main>
   <router-view/>
+  </v-main>
 </v-app>
 </template>
--- ricardsbubisslv/src/main.ts
+++ ricardsbubisslv/src/main.ts
@@ -10,12 +10,23 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 
 const app = createApp(App)
 
 const vuetify = createVuetify({
     components,
-    directives
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+    theme: {
+        defaultTheme: 'light'
+    }
 })
 
 app.use(createPinia())
